38970c71191f90836221193e578a73bcb89493ce
dataweb <bert@data-web.be>
2022-11-08 21:59:10 +0100

    Update php_functies.php

--- programmeren/php_functies.php
+++ programmeren/php_functies.php
@@ -3,33 +3,19 @@
 $zin = "Een zin in kleur.";
 $kleuren_opties = ['blue', 'green', 'red'];
 function zinInRandomKleur($zin, $kleuren_opties) {
-    // Aantal kleuren in de array (voor de bovengrens van de random functie).
-    $aantal_kleuren = count($kleuren_opties);
+    $random_kleur = bepaalRandomKleur($kleuren_opties);
 
-    // Bepaal een random getal tussen 0 en $aantal_kleuren minus 1.
-    $random_keuze = rand(0, $aantal_kleuren - 1);
-
-    // De random kleur.
-    $random_kleur = $kleuren_opties[$random_keuze];
-
-    return '<span style="color: ' . $random_kleur. '">Een zin in kleur</span>';
+    return '<span style="color: ' . $random_kleur. '">' . $zin. '</span>';
 }
 
 function woordenInRandomKleur($zin, $kleuren_opties) {
     $woorden_in_random_kleur = '';
 
-    // Aantal kleuren in de array (voor de bovengrens van de random functie).
-    $aantal_kleuren = count($kleuren_opties);
-
     // Transformeer de woorden in de zin naar een array:
     // $woorden = ['Een', 'zin', 'in', kleur.'];
     $woorden = explode(' ', $zin);
     foreach ($woorden as $woord) {
-       // Bepaal een random getal tussen 0 en $aantal_kleuren minus 1.
-       $random_keuze = rand(0, $aantal_kleuren - 1);
-
-       // De random kleur.
-       $random_kleur = $kleuren_opties[$random_keuze];
+        $random_kleur = bepaalRandomKleur($kleuren_opties);
 
        // De uitvoer.
        $woorden_in_random_kleur .= '<span style="color: ' . $random_kleur. '">' . $woord. '</span> ';
@@ -39,7 +25,34 @@ function woordenInRandomKleur($zin, $kleuren_opties) {
 }
 
 function lettersInRandomKleur($zin, $kleuren_opties) {
-    //...
+    $letters_in_random_kleur = '';
+
+    // Aantal kleuren in de array (voor de bovengrens van de random functie).
+    $aantal_kleuren = count($kleuren_opties);
+
+    // $letters = ['E', 'e', 'n', ' ', 'z', 'i', 'n', ' ', 'k', 'l', 'e', 'u', 'r'];
+    $letters = str_split($zin, 1);
+    foreach($letters as $letter) {
+       $random_kleur = bepaalRandomKleur($kleuren_opties);
+
+       // De uitvoer.
+       $letters_in_random_kleur .= '<span style="color: ' . $random_kleur. '">' . $letter. '</span>';
+    }
+
+    return $letters_in_random_kleur;
+}
+
+function bepaalRandomKleur($kleuren_opties) {
+    // Aantal kleuren in de array (voor de bovengrens van de random functie).
+    $aantal_kleuren = count($kleuren_opties);
+
+    // Bepaal een random getal tussen 0 en $aantal_kleuren minus 1.
+    $random_keuze = rand(0, $aantal_kleuren - 1);
+
+    // De random kleur.
+    $random_kleur = $kleuren_opties[$random_keuze];
+
+    return $random_kleur;
 }
 ?>
 
